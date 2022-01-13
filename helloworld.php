000de0e2cb76c16e301a5386c3df4193d1472380
TerryChan28888 <43136888+TerryChan28888@users.noreply.github.com>
2022-01-13 17:01:26 +0800

    Initial commit

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<?php
+//echo "<br />";
+//echo nl2br("\n");
+echo "Tomorrow I'll learn PHP global variables.";
+echo nl2br("\n");
+echo "This is a bad command : del c:\*.* ";
+
+$var = 'PHP Tutorial';
+echo "<h3>".$var."</h3>";
+
+?>
